a1dcd883d08709741f2be24873e4a8135921c85d
amineh-a <amineh@mit.edu>
2019-10-04 11:58:30 -0400

    added om_run.sh

---
+++ om_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 3
+#SBATCH --array=0-4
+#SBATCH --job-name=dqn
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:tesla-k80:1
+#SBATCH -t 24:00:00
+#SBATCH -D /om/user/amineh/rat_exp/log/
+#SBATCH --partition=cbmm
+
+cd /om/user/amineh/rat_agent/
+singularity exec -B /om:/om --nv /om/user/xboix/singularity/xboix-tensorflow1.14.simg \
+python3 main.py --agent dqn_gym --host_filesystem om --run train --experiment_index ${}
+
+
